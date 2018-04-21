1eeb00d1b768ba0dedfaf5d89593352d23b0f7d5
Pieter-Bas IJdens <pieter-bas.ijdens@25more.nl>
2018-04-21 10:39:09 +0200

    Refactored to create one single simple package

---
+++ package_solution.bat
@@ -0,0 +1,4 @@
+rem dotnet pack --runtime ubuntu.16.04-x64 --configuration Release Botje.Messaging\Botje.Messaging.csproj
+rem dotnet pack --runtime win-x64 --configuration Release Botje.Messaging\Botje.Messaging.csproj
+rem dotnet pack --runtime win-x86 --configuration Release Botje.Messaging\Botje.Messaging.csproj
+dotnet pack --configuration Release Botje.Messaging\Botje.Messaging.csproj
